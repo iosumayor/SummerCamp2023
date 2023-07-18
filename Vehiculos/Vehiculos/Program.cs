572c320de296500e867a80813dd01c87ac2308ee
iosumayor <iosumayor23@gmail.com>
2023-07-18 13:33:01 +0200

    con vehiculos

---
+++ Vehiculos/Vehiculos/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Vehiculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var coche1 = new Vehiculo();
+            coche1.marca = "ferrari";
+            coche1.valorbase = 1000;
+            coche1.antiguedad = 2021;
+            coche1.etiqueta = "sinetiqueta";
+
+            var coche2 = new Vehiculo();
+            coche2.marca = "opel";
+            coche2.valorbase = 1000;
+            coche2.antiguedad = 1998;
+            coche2.etiqueta = "etiquetaB";
+
+            var coche3 = new Vehiculo();
+            coche3.marca = "fiat";
+            coche3.valorbase = 1000;
+            coche3.antiguedad = 1996;
+            coche3.etiqueta = "etiquetaECO";
+
+            var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            numeros.Add(9);
+
+            var lista = new List<Vehiculo>();
+            lista.Add(coche1);
+            lista.Add(coche2);
+            lista.Add(coche3);
+
+            foreach (var vehiculo in lista)
+            {
+                //
+                vehiculo.CalcularAntiguedad();
+                vehiculo.CalcularEtiqueta();
+
+            }
+            Console.WriteLine(coche1.valorbase);
+            Console.WriteLine(coche2.valorbase);
+            Console.WriteLine(coche3.valorbase);
+
+        }
+    }
+}
---
+++ Vehiculos/Vehiculos/Vehiculo.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Vehiculos
+{
+    internal class Vehiculo
+    {
+        DateTime P = DateTime.Now;
+        int x;
+        int pepe;
+        //public enum Etiquetaeco
+        //{
+        //    sinetiqueta, etiquetaB,etiquetaC,etiquetaECO,etiquetaCERO
+        //}
+
+        public Vehiculo( string marca, int valorbase,int antiguedad, string etiqueta) 
+        {
+            this.marca = marca;
+            this.valorbase = valorbase;
+            this.antiguedad = antiguedad;
+            this.etiqueta = etiqueta;
+        }
+
+        public Vehiculo()
+        {
+        }
+
+        public string marca { get; set; }
+        public int valorbase { get; set; }
+        public int antiguedad { get; set; }
+        public string etiqueta { get; set; }
+
+        internal void CalcularAntiguedad()
+        {
+            var x = DateTime.Now.Year - antiguedad;
+            double pepe = valorbase+( x * 0.01);
+            Console.WriteLine(pepe);
+        }
+        //holalaaheufgwufbfwbjfwbjf
+        internal void CalcularEtiqueta()
+        {
+            switch (etiqueta)
+            {
+                case "sinetiqueta":
+                    valorbase = (int)(valorbase + (valorbase * 0.25));
+                    break;
+                case "etiquetaB":
+                    valorbase = (int)(valorbase + (valorbase * 0.15));
+                    break;
+                case "etiquetaC":
+                    valorbase = (int)(valorbase + (valorbase * 0.10));
+                    break;
+                case "etiquetaECO":
+                    valorbase = (int)(valorbase + (valorbase * 0.05));
+                    break;
+                case "etiquetaCERO":
+                    valorbase = (int)(valorbase);
+                    break;
+
+                    
+
+            }
+        }
+    }
+}
