972702032de9f4c269286573add46ac8a47c7315
pmpcurso1 <iosumayor23@gmail.com>
2023-07-18 12:42:09 +0200

    Commit inicial

---
+++ ejercicio/ejercicio/Entidades/Empleado.cs
@@ -0,0 +1,49 @@
+﻿namespace Entidades
+{
+    //public - Visible en todo el proyecto
+    //private - No es visible
+    //protected - 
+    //internal
+    public class Empleado
+    {
+        public enum Nivel
+        {
+            nomal,
+            bueno,
+            excelente
+        }
+        private Nivel _nivel;
+
+        public void FijarNivel(Nivel nivel)
+        {
+            this._nivel = nivel;
+        }
+
+        public Empleado()
+        {
+
+        }
+
+        public Empleado(string Nombre, double Salario, DateTime FechaAlta, bool Alta)
+        {
+            this.Nombre = Nombre;
+            this.Salario = Salario;
+            this.FechaAlta = FechaAlta;
+            this.Alta = Alta;
+        }
+
+        public string Nombre { get; set; }
+
+        public double Salario { get; set; }
+
+        public DateTime FechaAlta { get; set; } = DateTime.Now;
+
+        public bool Alta { get; set; }
+
+        public void CalcularAumentoSlario()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ejercicio/ejercicio/ejercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿using Entidades;
+
+namespace ejercicio
+{
+    //Calcular el salario empleados
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Jose = new Empleado();
+            Jose.Nombre = "Jose";
+            Jose.FechaAlta = new DateTime(2021, 01, 10);
+            Jose.Salario = 20000;
+            Jose.Alta = true;
+            Jose.FijarNivel(Empleado.Nivel.excelente);
+
+            // Crear y asignar valores a propiedades directamente
+            var Maria = new Empleado()
+            {
+                Nombre = "Maria",
+                FechaAlta = new DateTime(2022, 03, 01),
+                Salario = 20000,
+                Alta = true
+            };
+            Maria.FijarNivel(Empleado.Nivel.excelente);
+
+            var Juan = new Empleado("Juan", 20000, new DateTime(2023, 01, 15),true);
+            Juan.FijarNivel(Empleado.Nivel.excelente);
+
+            var numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            numeros.Add(9);
+
+            var lista = new List<Empleado>();
+            lista.Add(Maria);
+            lista.Add(Juan);
+            lista.Add(Jose); 
+
+            foreach (var empleado in lista) {
+
+                empleado.CalcularAumentoSlario();
+
+            }
+        }
+    }
+}
