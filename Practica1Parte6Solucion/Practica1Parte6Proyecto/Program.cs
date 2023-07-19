ddfb19a90dc5892f712c5bffafae90238a03dee3
iosumayor <iosumayor23@gmail.com>
2023-07-19 13:35:37 +0200

    meto la practica1 parte 6

---
+++ Practica1Parte6Solucion/Practica1Parte6Proyecto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Practica1Parte6Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string marca = String.Empty;
+            string modelo = String.Empty;
+            string color = String.Empty;
+            int year ;
+            int i;
+
+            Console.WriteLine("Cuantos coches vas a almacenar: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= j; i++)
+            {
+                Console.Write("Dame la marca del coche: ");
+                marca = Console.ReadLine();
+                Console.Write("Dame el modelo del coche: ");
+                modelo = Console.ReadLine();
+                Console.Write("Dame el color del coche: ");
+                color = Console.ReadLine();
+                Console.Write("Dame el año de compra: ");
+                year = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                Vehiculo vehiculo = new Vehiculo();
+                {
+                    vehiculo.FechaCompra = year;
+                    vehiculo.Marca = marca;
+                    vehiculo.Modelo = modelo;
+                    vehiculo.Color = color;
+
+                }
+
+                Console.WriteLine($"Tu coche es : {marca} , {modelo}, {color}, {year} ");
+            }
+
+            
+
+            
+        }
+    }
+}
---
+++ Practica1Parte6Solucion/Practica1Parte6Proyecto/Vehiculo.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Practica1Parte6Proyecto
+{
+    public class Vehiculo
+    {
+        public int FechaCompra { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; } 
+        public string Color { get; set; }
+
+
+        public Vehiculo(int FechaCompra, string Modelo, string Marca,string Color)
+        {
+            this.FechaCompra = FechaCompra;
+            this.Modelo = Modelo;
+            this.Marca = Marca;
+            this.Color = Color;
+        }
+
+
+
+        public Vehiculo() { 
+        
+        }
+    }
+}
