c51a53d0ee0cf26c25146760dd3a790dd0d22712
iosumayor <iosumayor23@gmail.com>
2023-07-19 12:43:39 +0200

    Introduzco la practica 2 de la parte 6

---
+++ Practica2Parte6Solucion/Practica2Parte6Proyecto/MonedasConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2Parte6Proyecto
+{
+    public class MonedasConverter
+
+        ///voy creando conversores para cada uno y que me devulevan la moneda que quiero
+    {
+        public static double EurosToDolares(string monEuros)
+        {
+            double euros = Double.Parse(monEuros);
+            double dolares = euros * 1.12;
+            return dolares;
+        }
+
+        public static double DolaresToEuros(string monDolares)
+        {
+            double dolares = Double.Parse(monDolares);
+            double euros = dolares * 0.89;
+            return euros;
+        }
+
+
+        public static double EurosToLibras(string monEuros)
+        {
+            double euros = Double.Parse(monEuros);
+            double libras = euros * 0.87;
+            return libras;
+        }
+
+        public static double LibrasToEuros(string monLibras)
+        {
+            double libras = Double.Parse(monLibras);
+            double euros = libras * 1.15;
+            return euros;
+        }
+
+        public static double DolaresToLibras(string monDolares)
+        {
+            double dolares = Double.Parse(monDolares);
+            double libras = dolares * 0.77;
+            return libras;
+        }
+
+        public static double LibrasToDolares(string monLibras)
+        {
+
+            double libras = Double.Parse (monLibras);
+            double Dolares = libras * 1.30;
+            return Dolares;
+        }
+
+
+
+    }
+}
---
+++ Practica2Parte6Solucion/Practica2Parte6Proyecto/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Practica2Parte6Proyecto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection = String.Empty;///dejo una seleccion vacia para poder seleccionar la opcion
+
+            while (selection != "c" && selection !="C") {
+
+                Console.Write("Introduce E) para elegir Euros , Introduce D) para elegir Dolares ,Introduce L) para elegir Libras o Introduce C) para cerrar:  ");
+
+                selection = Console.ReadLine();
+
+                double euros, dolares, libras = 0;///tener las variables iniciadas
+
+                
+                
+
+
+                switch (selection)
+                {
+                    case "E":
+                    case "e":
+                        Console.Write("Introduce D) para cambiar a Dolares, Introduce L) para cambiar a Libras: ");
+
+                        string eurosSelection = String.Empty;
+                        eurosSelection = Console.ReadLine();
+
+
+                        switch (eurosSelection)
+                        {
+                            case "D":
+                            case "d":
+                                Console.Write("Introduce la cantidad de Euros: ");
+                                dolares = MonedasConverter.EurosToDolares(Console.ReadLine());
+                                Console.WriteLine("Tienes : " + dolares +" dolares");
+                                break;
+
+                            case "L":
+                            case "l":
+                                Console.Write("Introduce la cantidad de Euros: ");
+                                libras = MonedasConverter.EurosToLibras(Console.ReadLine());
+                                Console.WriteLine($"Tienes : {libras:f2} libras");
+                                break;
+
+                            default:
+                                Console.WriteLine("Intente otra vez ");
+                                break;
+                                
+
+                               
+
+                        }
+                     break;
+
+                    case "D":
+                    case "d":
+
+                        Console.Write("Introduce E) para cambiar a Euros, Introduce L) para cambiar a Libras: ");
+
+                        string dolarSelection = String.Empty;
+                        dolarSelection = Console.ReadLine();
+
+                        switch (dolarSelection)
+                        {
+                            case "E":
+                            case "e":
+                                Console.Write("Introduce la cantidad de Dolares: ");
+                                euros = MonedasConverter.DolaresToEuros(Console.ReadLine());
+                                Console.WriteLine($"Tienes : {euros:f2} euros");
+                                break;
+
+                            case "L":
+                            case "l":
+                                Console.Write("Introduce la cantidad de Dolares: ");
+                                libras = MonedasConverter.DolaresToLibras(Console.ReadLine());
+                                Console.WriteLine($"Tienes : {libras:f2} libras");
+                                break;
+
+                            default:
+                                Console.WriteLine("Intente otra vez ");
+                                break;
+                        }
+                        break;
+
+                    case "L":
+                    case "l":
+
+                        Console.Write("Introduce E) para cambiar a Euros , Introduce D) para cambiar a Dolares: ");
+
+                        string libraSelection = String.Empty;
+                        libraSelection = Console.ReadLine();
+
+                        switch (libraSelection)
+                        {
+
+                            case "E": 
+                            case "e":
+                                Console.Write("Introduce la cantidad de Euros: ");
+                                euros = MonedasConverter.LibrasToEuros(Console.ReadLine());
+                                Console.WriteLine($"Tienes : {euros:f2} euros");
+                                break;
+
+                            case "D":
+                            case "d":
+                                Console.Write("Introduce la cantidad de Dolares: ");
+                                dolares = MonedasConverter.LibrasToDolares(Console.ReadLine());
+                                Console.WriteLine($"Tienes : {dolares:f2} dolares");
+                                break;
+
+                            default:
+                                Console.WriteLine("Intente otra vez ");
+                                break;
+                                ;                        }
+
+                    break;
+
+                    default:
+                        Console.WriteLine("Intente otra vez.");
+                    break;
+                }
+
+            
+            }
+        }
+    }
+}
