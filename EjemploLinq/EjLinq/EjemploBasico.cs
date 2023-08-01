118e657476bb8f1b348988768f0522d4061620dc
iosumayor <iosumayor23@gmail.com>
2023-08-01 12:23:15 +0200

    meto lo del 1/08

---
+++ EjemploLinq/EjLinq/EjemploBasico.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjLinq
+{
+    public class EjemploBasico
+    {
+
+        public EjemploBasico() { 
+        
+        }
+
+        public void Ejecutar()
+        {
+            string[] niveles = { "Basico", "Intermedio", "Avanzado" };
+            int nc = niveles.Count();
+
+            // Selecciona todos los niveles cuya longitud en caracteres
+            //sea mayor que 6
+            //ordenado por nivel
+
+            var listaConsultaForEach = new List<string>();
+
+            foreach (string nivel in niveles)
+            {
+
+                if(nivel.Length > 6)
+                {
+                    listaConsultaForEach.Add(nivel);
+                }
+
+            }
+
+            //Sintaxis SQL
+            //Select nivel from niveles where  nivel.length > 6 order by niveles
+            //1-Preparacion
+            var consultaLinq = from nivel in niveles where nivel.Length > 6 orderby nivel select nivel;
+
+            //2-Obtener Resultados
+            List<string> listaResultados = consultaLinq.ToList();//Convertir a una lista la info que nos llega para poder tratarla
+
+            //Sintaxis de Metodos
+            //1-Preparacion
+            Func<string, bool> predicado = nivel => nivel.Length > 6;
+
+            //var consultaLinqMetodos = niveles.Where(predicado);
+            var consultaLinqMetodos = niveles.Where(nivel => nivel.Length > 6).OrderBy( nivel => nivel).Select(nivel => nivel) ;
+            //2-Resultados
+
+
+            List<string> listaResultadosLinqMetodos = consultaLinqMetodos.ToList();
+
+        }
+
+    }
+}
---
+++ EjemploLinq/EjLinq/EjemploOperador1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjLinq
+{
+    internal class EjemploOperador1
+    {
+        public EjemploOperador1()
+        {
+
+        }
+        internal void Ejecutar()
+        {
+
+            // Lista de empleados
+            IEnumerable<Empleado> Empleados = new List<Empleado>()         {             new Empleado             {                 Nombre = "Jose",                 Apellidos = "Conde",                 Ciudad = "Madrid",                 Telefono = "123456789",                 Departamento = Departamento.Desarrollo             },             new Empleado             {                 Nombre = "Luis",                 Apellidos = "Castillo",                 Ciudad = "Madrid",                 Telefono = "423456789",                 Departamento = Departamento.Soporte             },             new Empleado             {                 Nombre = "Juan",                 Apellidos = "Nuñez",                 Ciudad = "Barcelona",                 Telefono = "9123456789",                 Departamento= Departamento.RH             },             new Empleado             {                 Nombre = "Maria",                 Apellidos = "Garcia",                 Ciudad = "Valencia",                 Telefono = "8123456789",                 Departamento = Departamento.Admin             }          };
+
+
+            var empleadosDesarrolloSoporte = Empleados.Where(empleados =>
+                                            empleados.Departamento == Departamento.Desarrollo || empleados.Departamento == Departamento.Soporte && empleados.Apellidos.StartsWith("C"))
+            .OrderByDescending(empleados => empleados.Nombre).ToList();
+
+            var empleadosDesarrolloSoporteConsulta = (from empleado in Empleados
+                                                     where (empleado.Departamento == Departamento.Desarrollo || empleado.Departamento == Departamento.Soporte) && empleado.Apellidos.StartsWith("C")
+                                                     orderby empleado.Nombre descending
+                                                     select empleado ).ToList();
+
+
+            //Listado de los telefonos de los empleados de Madrid
+            // que contengan en su apellido una "a"
+            //ordenado por nombre
+
+
+            var empleadosTelefonoMadrid = Empleados.Where(e =>
+                                                             e.Ciudad == "Madrid" &&
+                                                             e.Apellidos.Contains("a"))
+                                      .OrderBy(e => e.Nombre)
+                                      .Select(e => e.Telefono)
+                                      .ToList();
+
+
+            var empleadosTelefonoMadridSql = (from empleado in Empleados where (empleado.Apellidos.StartsWith("A")) orderby empleado.Nombre ascending select empleado.Telefono.ToList());
+
+
+            var Empleadosoportelefono1 = (from empleado in Empleados
+                                          where (empleado.Apellidos.StartsWith("C"))                                          orderby empleado.Nombre ascending                                          select new { Telefono = empleado.Telefono, Ciudad = empleado.Ciudad }).ToList();
+
+
+            //Agregar esta lista a los antiguos emplados
+            // No se permite el uso de lista.add
+            // Se agregan todos de una vez
+
+            var empleadosNuevos = new List<Empleado>
+            {
+                new Empleado
+                {
+                    Nombre = "Fabricio",
+                    Apellidos = "Cordero",
+                    Departamento = Departamento.Desarrollo
+                },
+
+                new Empleado
+                {
+                    Nombre = "Julia",
+                    Apellidos = "Lombardo",
+                    Departamento = Departamento.Admin
+                },
+            };
+
+
+            List<Empleado> empladosAntiguos = Empleados                                           .Select(empleado => empleado)                                           .ToList();            empladosAntiguos.AddRange(empleadosNuevos);
+
+            
+            Empleados = Empleados.Concat(empleadosNuevos).ToList();
+            
+            foreach(var empleadillo in Empleados)//foreach para recorrer todo los empleados
+            {
+                Console.WriteLine(empleadillo.Nombre);
+            }
+            
+
+            
+        }
+
+        
+
+
+
+    }
+}
